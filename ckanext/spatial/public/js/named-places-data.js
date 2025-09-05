5b0d1356fefc2458a0ab11004d606876739ee23a
rzmk <30333942+rzmk@users.noreply.github.com>
2025-09-05 16:28:51 -0400

    feat: public map for USA and refactor

--- ckanext/spatial/public/js/named-places-data.js
+++ ckanext/spatial/public/js/named-places-data.js
@@ -1,13 +1,5 @@
 window.__named_places = {
-    "Cities": "TX_Cities.json",
-    "Counties": "TX_Counties.json",
-    "Flood Planning Regions": "Flood_Planning_Regions.json",
-    "Groundwater Conservation Districts": "GW_Conservation_Districts.json",
-    "Major Aquifers": "Major_Aquifers.json",
-    "Major River Basins": "Major_River_Basins.json",
-    "Minor Aquifers": "Minor_Aquifers.json",
-    "Public Water Systems": "Public_Water_Systems.json",
-    "River Authorities": "River_Authorities.json",
-    "Statewide": "Statewide.json",
-    "Water Planning Regions": "Water_Planning_Regions.json"
+    "States": "States.json",
+    "Counties": "Counties.json",
+    "Nation": "Nation.json",
 };
